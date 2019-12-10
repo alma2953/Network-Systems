285cda93c796b953c3868a3fc482aa93ea6ec7ee
user <alma2953@colorado.edu>
2019-12-10 15:17:53 -0700

    added (mostly) completed PA4

--- PA4/dfc.c
+++ PA4/dfc.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <signal.h>
 #include <netdb.h>
 #include <dirent.h>
 #include <errno.h>
@@ -15,24 +16,33 @@
 
 #define MAXLINE 8192
 #define BUF 256
+#define MAXFILES 128
+#define MAXFILENAME 64
 
 #define DFS1 0
 #define DFS2 1
 #define DFS3 2
 #define DFS4 3
 
+char user[BUF];
+char pass[BUF];
 int sockfd[4];
 struct sockaddr_in serveraddr[4];
+bool serverUp = {true, true, true, true};
 
 void initServerDirs();
 void createSocket(int dfsIdx, int port);
-int sendtoDFS(char* msg, int dfsIdx);
+int sendtoDFS(char* msg, int dfsIdx, size_t len);
 int hashBucket(char* fname);
+void sigpipe_handler(int signum);
+void sendChunk(char* chunk, char* fname, char chunkChar, int dfsIdx, size_t len);
+int auth(char* user, char* pass, int dfsIdx);
+void printFiles(char* f1, char* f2, char* f3, char*f4);
+int hasChunk(char* fname, int chunk, int dfsIdx);
+void getChunk(int dfsIdx, int chunkSize, FILE* f);
 
 int main(int argc, char** argv){
   int port1, port2, port3, port4, n;
-  char user[BUF];
-  char pass[BUF];
   char userInput[BUF];
   char cmd[BUF];
   char fname[BUF];
@@ -73,30 +83,220 @@ int main(int argc, char** argv){
   fclose(conf);
   printf("Parsed file %s:\nDFS port 1: %d\nDFS port 2: %d\nDFS port 3: %d\nDFS port 4: %d\nUser: %s\nPass: %s\n", argv[1], port1, port2, port3, port4, user, pass);
 
-  createSocket(DFS1, port1);
-  createSocket(DFS2, port2);
-  createSocket(DFS3, port3);
-  createSocket(DFS4, port4);
+  signal(SIGPIPE, sigpipe_handler); //! Writes to a terminated server would cause a sigpipe signal, ignoring with custom handler
+
+  // createSocket(DFS1, port1);
+  // createSocket(DFS2, port2);
+  // createSocket(DFS3, port3);
+  // createSocket(DFS4, port4);
 
   while(1){ //Program loop
+
     bzero(userInput, BUF);
     printf("\nEnter one of the following commands:\n * get [filename]\n * put [filename]\n * ls\n> ");
     fgets(userInput, BUF, stdin);
     sscanf(userInput, "%s %s", cmd, fname);
 
-    if(!strcmp(cmd, "get")){
+    createSocket(DFS1, port1);
+    createSocket(DFS2, port2);
+    createSocket(DFS3, port3);
+    createSocket(DFS4, port4);
+
+    if(!strcmp(cmd, "put")){
       struct stat s;
+      ssize_t fsize;
       if(stat(fname, &s) != 0){
         printf("File %s does not exist.\n", fname);
         continue;
       }
       int bucket = hashBucket(fname);
       printf("Hash mod 4 is: %d\n", bucket);
-    } else if (!strcmp(cmd, "put")){
 
-    } else if (!strcmp(cmd, "ls")){
+      //Calculate file size
+      FILE* f = fopen(fname, "rb");
+      fseek(f, 0, SEEK_END);
+      fsize = ftell(f);
+      rewind(f);
 
-    } else {
+      char p1[fsize/4];
+      char p2[fsize/4];
+      char p3[fsize/4];
+      char p4[fsize - 3*(fsize/4)];
+
+      fread(p1, sizeof(p1), 1, f);
+      fread(p2, sizeof(p2), 1, f);
+      fread(p3, sizeof(p3), 1, f);
+      fread(p4, sizeof(p4), 1, f);
+      //printf("Piece 1: %s\nPiece 2: %s\nPiece 3: %s\nPiece 4: %s\n", p1, p2, p3, p4);
+
+      fclose(f);
+      if(bucket == 0){
+        sendChunk(p1, fname, '1', DFS1, sizeof(p1));
+        sendChunk(p2, fname, '2', DFS1, sizeof(p2));
+        sendChunk(p2, fname, '2', DFS2, sizeof(p2));
+        sendChunk(p3, fname, '3', DFS2, sizeof(p3));
+        sendChunk(p3, fname, '3', DFS3, sizeof(p3));
+        sendChunk(p4, fname, '4', DFS3, sizeof(p4));
+        sendChunk(p4, fname, '4', DFS4, sizeof(p4));
+        sendChunk(p1, fname, '1', DFS4, sizeof(p1));
+      } else if(bucket == 1){
+        sendChunk(p4, fname, '4', DFS1, sizeof(p4));
+        sendChunk(p1, fname, '1', DFS1, sizeof(p1));
+        sendChunk(p1, fname, '1', DFS2, sizeof(p1));
+        sendChunk(p2, fname, '2', DFS2, sizeof(p2));
+        sendChunk(p2, fname, '2', DFS3, sizeof(p2));
+        sendChunk(p3, fname, '3', DFS3, sizeof(p3));
+        sendChunk(p3, fname, '3', DFS4, sizeof(p3));
+        sendChunk(p4, fname, '4', DFS4, sizeof(p4));
+      } else if(bucket == 2){
+        sendChunk(p3, fname, '3', DFS1, sizeof(p3));
+        sendChunk(p4, fname, '4', DFS1, sizeof(p4));
+        sendChunk(p4, fname, '4', DFS2, sizeof(p4));
+        sendChunk(p1, fname, '1', DFS2, sizeof(p1));
+        sendChunk(p1, fname, '1', DFS3, sizeof(p1));
+        sendChunk(p2, fname, '2', DFS3, sizeof(p2));
+        sendChunk(p2, fname, '2', DFS4, sizeof(p2));
+        sendChunk(p3, fname, '3', DFS4, sizeof(p3));
+      } else {
+        sendChunk(p2, fname, '2', DFS1, sizeof(p2));
+        sendChunk(p3, fname, '3', DFS1, sizeof(p3));
+        sendChunk(p3, fname, '3', DFS2, sizeof(p3));
+        sendChunk(p4, fname, '4', DFS2, sizeof(p4));
+        sendChunk(p4, fname, '4', DFS3, sizeof(p4));
+        sendChunk(p1, fname, '1', DFS3, sizeof(p1));
+        sendChunk(p1, fname, '1', DFS4, sizeof(p1));
+        sendChunk(p2, fname, '2', DFS4, sizeof(p2));
+      }
+    }
+
+    else if (!strcmp(cmd, "ls")){
+      char files1[MAXLINE];
+      char files2[MAXLINE];
+      char files3[MAXLINE];
+      char files4[MAXLINE];
+      memset(files1, 0, MAXLINE);
+      memset(files2, 0, MAXLINE);
+      memset(files3, 0, MAXLINE);
+      memset(files4, 0, MAXLINE);
+
+      int authval = auth(user, pass, DFS1);
+      if(authval == 1){
+        sendtoDFS("ls", DFS1, 2);
+        n = read(sockfd[DFS1], files1, MAXLINE);
+        if(n <= 0){
+          printf("DFS1 is down.\n");
+        }
+
+        if(!strcmp(files1, "nf")){
+          printf("No files on DFS1.\n");
+          memset(files1, 0, MAXLINE);
+        }
+      } else if(authval == 0){
+        printf("Authentication failed.\n");
+        continue;
+      }
+
+      authval = auth(user, pass, DFS2);
+      if(authval == 1){
+        sendtoDFS("ls", DFS2, 2);
+        n = read(sockfd[DFS2], files2, MAXLINE);
+        if(n <= 0){
+          printf("DFS2 is down.\n");
+        }
+
+        if(!strcmp(files2, "nf")){
+          printf("No files on DFS2.\n");
+          memset(files2, 0, MAXLINE);
+        }
+      } else if(authval == 0){
+        printf("Authentication failed.\n");
+        continue;
+      }
+
+      authval = auth(user, pass, DFS3);
+      if(authval == 1){
+        sendtoDFS("ls", DFS3, 2);
+        n = read(sockfd[DFS3], files3, MAXLINE);
+        if(n <= 0){
+          printf("DFS3 is down.\n");
+        }
+
+        if(!strcmp(files3, "nf")){
+          printf("No files on DFS3.\n");
+          memset(files3, 0, MAXLINE);
+        }
+      } else if(authval == 0){
+        printf("Authentication failed.\n");
+        continue;
+      }
+
+      authval = auth(user, pass, DFS4);
+      if(authval == 1){
+        sendtoDFS("ls", DFS4, 2);
+        n = read(sockfd[DFS4], files4, MAXLINE);
+        if(n <= 0){
+          printf("DFS4 is down.\n");
+        }
+
+        if(!strcmp(files4, "nf")){
+          printf("No files on DFS4.\n");
+          memset(files4, 0, MAXLINE);
+        }
+      } else if(authval == 0){
+        printf("Authentication failed.\n");
+        continue;
+      }
+
+    printFiles(files1, files2, files3, files4);
+    }
+
+    else if (!strcmp(cmd, "get")){
+      if(auth(user, pass, DFS1) == 0){
+        printf("Authentication failed.\n");
+        continue;
+      }
+      if(auth(user, pass, DFS2) == 0){
+        printf("Authentication failed.\n");
+        continue;
+      }
+      if(auth(user, pass, DFS3) == 0){
+        printf("Authentication failed.\n");
+        continue;
+      }
+      if(auth(user, pass, DFS4) == 0){
+        printf("Authentication failed.\n");
+        continue;
+      }
+
+      FILE* f;
+      f = fopen(fname, "wb");
+      if(!f){
+        printf("Error opening file %s\n", fname);
+        continue;
+      }
+      for(int chonk = 1; chonk < 5; chonk++){ //For each chunk
+        bool chunkExists = false;
+        for(int dfs = 0; dfs < 4; dfs++){ //For each server
+          int chunkSize = hasChunk(fname, chonk, dfs);
+          if(chunkSize > 0){
+            chunkExists = true;
+            getChunk(dfs, chunkSize, f);
+            break;
+          }
+          if(chunkSize == 0){
+            //printf("The requested file does not exist on DFS%d", dfs+1);
+          }
+        }
+        if(!chunkExists){
+          printf("File is incomplete.\n");
+          remove(fname);
+          break;
+        }
+      }
+      fclose(f);
+    }
+
+    else {
       printf("Invalid command.\n");
     }
   }
@@ -121,14 +321,23 @@ void initServerDirs(){
   }
 }
 
+
+
 //! Initializes socket for DFS<dfsIdx+1> on port# <port>
 void createSocket(int dfsIdx, int port){
   struct hostent* server;
+  struct timeval timeout;
+  timeout.tv_sec = 1;
+  timeout.tv_usec = 0;
 
   sockfd[dfsIdx] = socket(AF_INET, SOCK_STREAM, 0);
   if(sockfd[dfsIdx] < 0){
     printf("Error creating socket\n");
     exit(1);
+  }
+
+  if(setsockopt(sockfd[dfsIdx], SOL_SOCKET, SO_RCVTIMEO, (char*)&timeout, sizeof(timeout)) < 0){
+    printf("Error setting timeout\n");
   }
 
   server = gethostbyname("127.0.0.1");
@@ -145,24 +354,253 @@ void createSocket(int dfsIdx, int port){
   serveraddr[dfsIdx].sin_port = htons(port);
 
   if(connect(sockfd[dfsIdx], (struct sockaddr*)&serveraddr[dfsIdx], sizeof(struct sockaddr_in)) != 0){
-    printf("Error in connect: errno %d\n", errno);
-    exit(1);
+    //printf("Error in connect: errno %d\n", errno);
+    //exit(1);
+  }
+}
+
+//Returns 0 if the file does not exist on the dfs, else returns file size
+int hasChunk(char* fname, int chunk, int dfsIdx){
+  int n;
+  char cmdbuf[strlen(fname) + strlen("get ") + strlen(".1 ")];
+  memset(cmdbuf, 0, sizeof(cmdbuf));
+  char buf[BUF];
+  char chunkChar = chunk + '0';
+  strcpy(cmdbuf, "get ");
+  strcat(cmdbuf, fname);
+  strcat(cmdbuf, ".");
+  strncat(cmdbuf, &chunkChar, 1);
+
+  printf("Sending command: %s\n", cmdbuf);
+
+  sendtoDFS(cmdbuf, dfsIdx, strlen(cmdbuf));
+
+  memset(buf, 0, sizeof(buf));
+  n = read(sockfd[dfsIdx], buf, BUF);
+  if(n <= 0){
+    printf("DFS%d is down.\n", dfsIdx+1);
+    return -1;
+  }
+  if(!strcmp(buf, "dne")){
+    printf("File %s is not present on DFS%d\n", fname, dfsIdx+1);
+    return 0;
+  }
+  printf("FILE EXISTS, SIZE %d BYTES\n", atoi(buf));
+  return atoi(buf);
+}
+
+void getChunk(int dfsIdx, int chunkSize, FILE* f){
+  char fileChunk[chunkSize];
+  memset(fileChunk, 0, chunkSize);
+  write(sockfd[dfsIdx], "ready", 5);
+  read(sockfd[dfsIdx], fileChunk, chunkSize);
+  printf("Received from server####:\n%s\n", fileChunk);
+  fwrite(fileChunk, 1, chunkSize, f);
+}
+
+void sendChunk(char* chunk, char* fname, char chunkChar, int dfsIdx, size_t len){
+  char fsize[BUF];
+  sprintf(fsize, "%d", (int)len);
+  char cmd[strlen(fname) + strlen("put ") + strlen(fsize) + 5]; //5 additional bytes for . prefix, .{1-4} suffix, and null terminator
+  int authRetval = auth(user, pass, dfsIdx);
+  if(authRetval != 1){
+    printf("Authentication failed.\n");
+    return;
+  }
+
+  strcpy(cmd, "put:");
+  strcat(cmd, fsize);
+  strcat(cmd, " ");
+  strcat(cmd, fname);
+  strcat(cmd, ".");
+  strncat(cmd, &chunkChar, 1);
+  printf("Sending command to DFS%d: %s\n", dfsIdx+1, cmd);
+
+  sendtoDFS(cmd, dfsIdx, strlen(cmd) + 1);
+
+  printf("Sending chunk, size: %d\n", (int)len);
+  sendtoDFS(chunk, dfsIdx, len);
+  printf("Sent chunk\n");
+}
+
+void printFiles(char* f1, char* f2, char* f3, char* f4){
+  char* tok;
+  int numFiles = 0;
+  char files[MAXFILES][MAXFILENAME]; //Stores a list of files
+  memset(files, 0, sizeof(files));
+  int fileParts[MAXFILES]; //Stores parts of file based on index in files table
+  memset(fileParts, 0, sizeof(fileParts));
+
+
+  if(strlen(f1) != 0){
+    tok = strtok(f1, "\n");
+    while(tok != NULL){
+      bool newFile = true;
+      //printf("tok: %s\n", tok);
+      char* chunk = strrchr(tok, '.')+1;
+      //printf("File chunk %s\n", chunk);
+      *(chunk-1) = '\0';
+
+      for(int i = 0; i < numFiles; i++){
+        if(!strcmp(tok, files[i])){
+          //printf("Found match for %s\n", tok);
+          newFile = false;
+          fileParts[i] |= 1<<(atoi(chunk)-1);
+          break;
+        }
+      }
+
+      if(newFile){
+        //printf("Adding file %s\n", tok);
+        strcpy(files[numFiles], tok);
+        fileParts[numFiles] |= 1<<(atoi(chunk)-1);
+        numFiles++;
+      }
+
+      tok = strtok(NULL, "\n");
+    }
+  }
+
+  if(strlen(f2) != 0){
+    tok = strtok(f2, "\n");
+    while(tok != NULL){
+      bool newFile = true;
+      //printf("tok: %s\n", tok);
+      char* chunk = strrchr(tok, '.')+1;
+      //printf("File chunk %s\n", chunk);
+      *(chunk-1) = '\0';
+
+      for(int i = 0; i < numFiles; i++){
+        if(!strcmp(tok, files[i])){
+          //printf("Found match for %s\n", tok);
+          newFile = false;
+          fileParts[i] |= 1<<(atoi(chunk)-1);
+          break;
+        }
+      }
+
+      if(newFile){
+        //printf("Adding file %s\n", tok);
+        strcpy(files[numFiles], tok);
+        fileParts[numFiles] |= 1<<(atoi(chunk)-1);
+        numFiles++;
+      }
+
+      tok = strtok(NULL, "\n");
+    }
+  }
+
+  if(strlen(f3) != 0){
+    tok = strtok(f3, "\n");
+    while(tok != NULL){
+      bool newFile = true;
+      //printf("tok: %s\n", tok);
+      char* chunk = strrchr(tok, '.')+1;
+      //printf("File chunk %s\n", chunk);
+      *(chunk-1) = '\0';
+
+      for(int i = 0; i < numFiles; i++){
+        if(!strcmp(tok, files[i])){
+          //printf("Found match for %s\n", tok);
+          newFile = false;
+          fileParts[i] |= 1<<(atoi(chunk)-1);
+          break;
+        }
+      }
+
+      if(newFile){
+        //printf("Adding file %s\n", tok);
+        strcpy(files[numFiles], tok);
+        fileParts[numFiles] |= 1<<(atoi(chunk)-1);
+        numFiles++;
+      }
+
+      tok = strtok(NULL, "\n");
+    }
+  }
+
+  if(strlen(f4) != 0){
+    tok = strtok(f4, "\n");
+    while(tok != NULL){
+      bool newFile = true;
+      //printf("tok: %s\n", tok);
+      char* chunk = strrchr(tok, '.')+1;
+      //printf("File chunk %s\n", chunk);
+      *(chunk-1) = '\0';
+
+      for(int i = 0; i < numFiles; i++){
+        if(!strcmp(tok, files[i])){
+          //printf("Found match for %s\n", tok);
+          newFile = false;
+          fileParts[i] |= 1<<(atoi(chunk)-1);
+          break;
+        }
+      }
+
+      if(newFile){
+        //printf("Adding file %s\n", tok);
+        strcpy(files[numFiles], tok);
+        fileParts[numFiles] |= 1<<(atoi(chunk)-1);
+        numFiles++;
+      }
+
+      tok = strtok(NULL, "\n");
+    }
+  }
+
+  for(int i = 0; i < numFiles; i++){
+    printf("%s", files[i]);
+    if(fileParts[i] != 15){ //Binary 1111, indicating all 4 parts are present
+      printf(" [incomplete]");
+    }
+    printf("\n");
+  }
+}
+
+int auth(char* user, char* pass, int dfsIdx){
+  char authmsg[strlen("auth ") + strlen(user) + strlen(pass) + 2]; //2 additional bytes for ':' and null terminator
+  char response[BUF];
+  bzero(response, BUF);
+  ssize_t n;
+
+  //connect(sockfd[dfsIdx], (struct sockaddr*)&serveraddr[dfsIdx], sizeof(struct sockaddr_in));
+
+  strcpy(authmsg, "auth ");
+  strcat(authmsg, user);
+  strcat(authmsg, ":");
+  strcat(authmsg, pass);
+  //printf("Sending auth message to DFS%d: %s\n", dfsIdx+1, authmsg);
+  sendtoDFS(authmsg, dfsIdx, sizeof(authmsg));
+  n = read(sockfd[dfsIdx], response, BUF);
+  if(n <= 0){
+    printf("Auth error: DFS%d is down.\n", dfsIdx+1);
+    return -1;
+  }
+  if(!strcmp(response, "auth")){
+    //printf("User %s successfully authenticated\n", user);
+    return 1;
+  } else {
+    printf("Authentication failed for user %s\n", user);
+    return 0;
   }
 }
 
 //! Sends msg to specified DFS
-int sendtoDFS(char* msg, int dfsIdx){
+int sendtoDFS(char* msg, int dfsIdx, size_t len){
   int n;
 
-  n = write(sockfd[dfsIdx], msg, strlen(msg));
+  n = write(sockfd[dfsIdx], msg, len);
   if(n < 0){
-    printf("Error in sendto: errno %d\n", errno);
-    exit(1);
+    //printf("Error in sendto: errno %d\n", errno);
   }
   return n;
 }
 
-//! Returns md5sum() % 4 of file fname, implementation from https://stackoverflow.com/questions/10324611/how-to-calculate-the-md5-hash-of-a-large-file-in-c
+void sigpipe_handler(int signum){
+
+}
+
+//! Returns md5sum % 4 of file fname, implementation from https://stackoverflow.com/questions/10324611/how-to-calculate-the-md5-hash-of-a-large-file-in-c
 int hashBucket(char* fname){
   unsigned char c[MD5_DIGEST_LENGTH];
   FILE* f = fopen(fname, "rb");
@@ -180,7 +618,7 @@ int hashBucket(char* fname){
     int md5Char = (int)c[i];
     modResult = (modResult*16 + md5Char) % 4;
   }
-  
-  fclose (f);
+
+  fclose(f);
   return modResult;
 }
--- PA4/dfs.c
+++ PA4/dfs.c
@@ -22,6 +22,8 @@ char path[6]; // stores local path, e.g "/DFS1"
 int open_listenfd(int port);
 void process(int connfd);
 void* thread(void* vargp);
+void createUserDir(char* user);
+bool auth(char* msg, char* userBuf);
 
 int main(int argc, char** argv){
   int listenfd, *connfdp, portno, clientlen=sizeof(struct sockaddr_in);
@@ -56,11 +58,11 @@ void* thread(void* vargp){
   free(vargp);
   process(connfd);
   close(connfd);
-  printf("Terminating thread\n");
   return NULL;
 }
 
 void process(int connfd){
+  char user[BUF];
   while(1){
     size_t n;
     char buf[MAXLINE];
@@ -70,6 +72,196 @@ void process(int connfd){
       break;
     }
     printf("Received:\n%s\n", buf);
+
+    if(!strncmp(buf, "auth", 4)){
+      if(auth(buf, user)){
+        write(connfd, "auth", 4);
+      } else {
+        write(connfd, "noauth", 6);
+      }
+    }
+
+    else if(!strncmp(buf, "get", 3)){
+      struct stat s;
+      int fsize;
+      char* fname = strchr(buf, ' ')+1;
+      char filepath[strlen(fname) + strlen(path) + strlen(user) + 4];
+      strcpy(filepath, ".");
+      strcat(filepath, path);
+      strcat(filepath, "/");
+      strcat(filepath, user);
+      strcat(filepath, "/");
+      strcat(filepath, fname);
+
+      createUserDir(user);
+      printf("Checking for existance of file %s\n", filepath);
+      if(stat(filepath, &s) == 0){
+        fsize = (int)s.st_size;
+        if(fsize == 0){
+          write(connfd, "dne", 3);
+          continue;
+        }
+        printf("File size: %d\n", fsize);
+        char str[32];
+        sprintf(str, "%d", fsize);
+        write(connfd, str, strlen(str));
+      } else {
+        write(connfd, "dne", 3);
+        continue;
+      }
+
+      char chunk[fsize];
+      memset(chunk, 0, fsize);
+      FILE* f = fopen(filepath, "rb");
+      if(!f){
+        printf("Error opening file###\n");
+      }
+      fread(chunk, fsize, 1, f);
+      fclose(f);
+
+      char ready_buf[5];
+      read(connfd, ready_buf, 5);
+      printf("Read file: \n%s\n", chunk);
+      write(connfd, chunk, fsize);
+    }
+
+    else if(!strncmp(buf, "put", 3)){
+      createUserDir(user);
+      char* fname = strchr(buf, ' ')+1;
+      *(fname - 1) = '\0';
+      char* fsize = strchr(buf, ':')+1;
+      int fileSize = atoi(fsize);
+      printf("File size is %d\n", fileSize);
+      printf("Got request put %s\n", fname);
+      char dir[strlen(path) +strlen(fname) + strlen(user) +  5]; //4 more bytes for slash before filename and user, dot prefix, and null terminator
+      bzero(dir, sizeof(dir));
+      strcpy(dir, ".");
+      strcat(dir, path);
+      strcat(dir, "/");
+      strcat(dir, user);
+      strcat(dir, "/");
+      strcat(dir, fname);
+
+      printf("Final dir is %s\n", dir);
+      FILE* f, *tst;
+      f = fopen(dir, "wb");
+      if(!f){
+        printf("Error opening file\n");
+        continue;
+      }
+
+      printf("1\n");
+      char filebuf[fileSize];
+      memset(filebuf, 0, sizeof(filebuf));
+      printf("2\n");
+      n = read(connfd, filebuf, fileSize);
+      printf("3\n");
+      printf("Received %d bytes:\n", (int)n);
+      if(n < 0){
+        printf("Error in put:read\n");
+        break;
+      }
+      printf("4\n");
+      fwrite(filebuf, 1, n, f);
+      memset(filebuf, 0, sizeof(filebuf));
+
+      fclose(f);
+    }
+
+    else if(!strncmp(buf, "ls", 2)){
+      createUserDir(user);
+
+      memset(buf, 0, sizeof(buf));
+      char fpath[strlen(path) + strlen(user) + 3];
+      memset(fpath, 0, sizeof(fpath));
+
+      strcpy(fpath, ".");
+      strcat(fpath, path);
+      strcat(fpath, "/");
+      strcat(fpath, user);
+
+      printf("Opening dir %s\n", fpath);
+      DIR* d = opendir(fpath);
+      while(true){
+        struct dirent* dir = readdir(d);
+        if(dir == NULL){
+          break;
+        }
+        if(strcmp(dir->d_name, ".") && strcmp(dir->d_name, "..")){ //Ignore "." and ".."
+          strcat(buf, dir->d_name);
+          strcat(buf, "\n");
+        }
+      }
+      closedir(d);
+      if(strlen(buf) == 0){
+        write(connfd, "nf", 2);
+      } else {
+        write(connfd, buf, strlen(buf));
+      }
+    }
+
+    else {
+      printf("Command not recognized\n");
+    }
+  }
+}
+
+//! Parses auth message msg, and stores username in user for future use
+bool auth(char* msg, char* userBuf){
+  char* line = NULL;
+  FILE* conf;
+  ssize_t len = 0;
+  ssize_t bytesRead;
+
+  char* user = strchr(msg, ' ') + 1;
+  char* pass = strchr(msg, ':') + 1;
+  *(pass-1) = '\0';
+
+  strncpy(userBuf, user, BUF);
+  printf("Checking dfc.conf for user:%s pass:%s\n", user, pass);
+
+  conf = fopen("dfs.conf", "r");
+  if(conf == NULL){
+    printf("Cannot open dfs.conf\n");
+    return false;
+  }
+
+  while((bytesRead = getline(&line, &len, conf)) != -1){
+    line[strcspn(line, "\n")] = 0; //Remove trailing '\n' if it exists
+    char* confPass = strchr(line, ' ') + 1;
+    *(confPass-1) = '\0';
+    if(!strcmp(pass, confPass) && !strcmp(user, line)){
+      printf("Match found, successfully authenticated user %s\n", line);
+      fclose(conf);
+      return true;
+    }
+  }
+  printf("Match not found, rejecting user %s\n", user);
+  fclose(conf);
+  return false;
+}
+
+void createUserDir(char* user){
+  DIR* d;
+  char dirpath[strlen(path) + strlen(user) + 3]; //3 bytes for preceding '.', slash before user, and null terminator
+
+  strcpy(dirpath, ".");
+  strcat(dirpath, path);
+  strcat(dirpath, "/");
+  strcat(dirpath, user);
+
+  printf("Checking existance of %s\n", dirpath);
+
+  d = opendir(dirpath);
+  if(d){
+    printf("Directory exists\n");
+  } else if(errno == ENOENT){
+    printf("Directory does not exist, creating...\n");
+    if(mkdir(dirpath, 0777) != 0){
+      printf("Error in mkdir: errno %d\n", errno);
+    }
+  } else {
+    printf("Opendir failed\n");
   }
 }
 
